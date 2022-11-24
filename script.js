746a632f902f5ed3697f3cab6bb4ca38ee11d9e3
Fábio Morais <fabio.gunner@live.com>
2022-11-24 12:29:48 -0300

    criacao do arquivo js, validação de login e inserção do texto de login inválido

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('#entrar').addEventListener('click', checkLogin)
-
+const iconUser = document.querySelector('.username')
+const iconPassword = document.querySelector('.userpassword')
 
 function checkLogin() {
     const email = document.querySelector('.email').value
@@ -8,12 +9,22 @@ function checkLogin() {
     const correctPassword = 'admin'
     if (email == correctEmail && password == correctPassword) {
         alert('Login realizado com sucesso!')
+        document.querySelector('.email').value = ''
+        document.querySelector('.password').value = ''
     } else {
-        document.querySelector('.invalid').innerHTML = '<p>Login ou senha inválidos!</p>'
-        setTimeout(() => {
-            document.querySelector('.invalid').innerHTML = ''
-            document.querySelector('.email').value = ''
-            document.querySelector('.password').value = ''
-        }, 2000);
+        wrongPassword()
     }
+}
+
+function wrongPassword() {
+    document.querySelector('.invalid').innerHTML = '<p>Login ou senha inválidos!</p>'
+    iconUser.style.color = '#FC575E'
+    iconPassword.style.color = '#FC575E'
+    setTimeout(() => {
+        document.querySelector('.invalid').innerHTML = ''
+        document.querySelector('.email').value = ''
+        document.querySelector('.password').value = ''
+        iconUser.style.color = '#F0F1F5'
+        iconPassword.style.color = '#F0F1F5'
+    }, 2000);
 }
